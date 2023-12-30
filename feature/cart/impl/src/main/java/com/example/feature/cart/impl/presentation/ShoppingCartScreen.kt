f088346b6820e835d22ea985ebcd6655a18d3a1d
GorlovDanila <danildanil20030@gmail.com>
2023-12-30 18:53:23 +0300

    add feature home and details

--- feature/cart/impl/src/main/java/com/example/feature/cart/impl/presentation/ShoppingCartScreen.kt
+++ feature/cart/impl/src/main/java/com/example/feature/cart/impl/presentation/ShoppingCartScreen.kt
@@ -1,23 +1,10 @@
 package com.example.feature.cart.impl.presentation
 
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
-import androidx.compose.material3.DrawerValue
-import androidx.compose.material3.ExtendedFloatingActionButton
-import androidx.compose.material3.Icon
-import androidx.compose.material3.ModalDrawerSheet
-import androidx.compose.material3.ModalNavigationDrawer
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.ui.Modifier
 import cafe.adriel.voyager.core.screen.Screen
 import cafe.adriel.voyager.navigator.LocalNavigator
 import cafe.adriel.voyager.navigator.currentOrThrow
-import kotlinx.coroutines.launch
 
 class ShoppingCartScreen : Screen {
 
